ac3821e79fc5dc143fb92d6e3665cb75cc80cd24
John Winston <59228178+winston0410@users.noreply.github.com>
2022-01-25 11:28:32 +0000

    set up storybook (#2)

---
+++ src/components/layout/GrayBox/GrayBox.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import GrayBox from './GrayBox';
+
+export default {
+  title: 'Layout/GrayBox',
+  component: GrayBox,
+  argTypes: {},
+} as ComponentMeta<typeof GrayBox>;
+
+const Template: ComponentStory<typeof GrayBox> = (args) => <GrayBox {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
