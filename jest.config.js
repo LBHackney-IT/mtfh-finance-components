36ab068fbf7b7fc98e3428715725858c9791a3aa
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-01-26 13:09:21 +0000

    Update based on review

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@ const customJestConfig = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
     coverageReporters: ['json-summary', 'text', 'lcov'],
-    collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+    collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.stories.tsx'],
     moduleNameMapper: {
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+        "\\.(css|scss|sass)$": "identity-obj-proxy"
     },
 };
 
