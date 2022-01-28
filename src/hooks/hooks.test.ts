725e7b448ca14946df6182e57a7309e9d42bfb34
John Winston <59228178+winston0410@users.noreply.github.com>
2022-01-28 11:32:51 +0000

    Migrate hooks (#8)
    * Migrate hooks
    
    * Fix type
    
    * Use generic from react-use
    
    * Add type declaration for jest spyon
    
    * Allow type error
    
    * Add tsignore

---
+++ src/hooks/hooks.test.ts
@@ -0,0 +1,36 @@
+import React from 'react';
+import type { ReactNode } from 'react';
+
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useTabs } from './index';
+
+const components = {
+  Block: 'block',
+  Estate: 'estate',
+};
+
+describe('useTabs', () => {
+  it('returns correct tabContent', () => {
+    const { result } = renderHook(() => useTabs(components));
+
+    expect(result.current.tabContent).toBe('block');
+  });
+
+  it('returns correct tabsProps', () => {
+    const setStateMock = jest.fn();
+    const useStateMock = (state: ReactNode) => [state, setStateMock];
+
+    // FIXME Type declaration for jest doesnt match with an original useState
+    //  @ts-ignore
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+
+    const { result } = renderHook(() => useTabs(components));
+
+    expect(result.current.tabsProps).toEqual({
+      changeTab: setStateMock,
+      currentTab: 'Block',
+      tabs: ['Block', 'Estate'],
+    });
+  });
+});
