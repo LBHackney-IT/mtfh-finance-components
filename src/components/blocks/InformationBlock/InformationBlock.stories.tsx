11fb66f6f8541eb8b122e83fb80c0de64905e872
kghugo <hugosum.dev@protonmail.com>
2022-02-01 09:00:50 +0000

    Add story for InformationBlock

---
+++ src/components/blocks/InformationBlock/InformationBlock.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import InformationBlock from './InformationBlock';
+
+export default {
+  title: 'Blocks/InformationBlock',
+  component: InformationBlock,
+  argTypes: {},
+} as ComponentMeta<typeof InformationBlock>;
+
+const Template: ComponentStory<typeof InformationBlock> = () => {
+  const data = [
+    {
+      id: 123,
+      value: 'foo',
+      label: 'bar',
+    },
+  ];
+
+  return (
+    <InformationBlock
+      header="Totals"
+      data={data}
+      icon="icon"
+      variant="main"
+      dataInRow
+    />
+  );
+};
+
+export const Normal = Template.bind({});
+Normal.args = {};
