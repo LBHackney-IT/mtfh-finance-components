e5ace30628d8244531e2375b060ee3b06eb93af6
kghugo <hugosum.dev@protonmail.com>
2022-02-01 11:28:28 +0000

    Add story and test for Accordion

--- src/components/ui/Accordion/Accordion.stories.tsx
+++ src/components/ui/Accordion/Accordion.stories.tsx
@@ -8,7 +8,9 @@ export default {
 } as ComponentMeta<typeof Accordion>;
 
 const Template: ComponentStory<typeof Accordion> = (args) => (
-  <Accordion {...args}>Click me</Accordion>
+  <Accordion {...args}>
+    <div>Click me</div>
+  </Accordion>
 );
 
 export const Normal = Template.bind({});
