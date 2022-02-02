f76933d57cab067caa70a0c953ab1fd4ad4ff374
kghugo <hugosum.dev@protonmail.com>
2022-02-02 20:09:05 +0000

    Add stories

---
+++ src/components/ui/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,30 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import Checkbox from './Checkbox';
+
+export default {
+  title: 'Ui/Checkbox',
+  component: Checkbox,
+  argTypes: {},
+} as ComponentMeta<typeof Checkbox>;
+
+const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />;
+
+const defaultArgs = {
+    title: "This is the title",
+    description: "This is the description",
+    name: "foo",
+    label: "bar",
+    onChange: () => {}
+}
+
+export const Checked = Template.bind({});
+Checked.args = {
+    checked: true,
+    ...defaultArgs
+};
+
+export const Unchecked = Template.bind({});
+Unchecked.args = {
+    checked: false,
+    ...defaultArgs
+};
