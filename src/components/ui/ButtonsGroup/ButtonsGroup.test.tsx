86d9ab7ce8417ea8ec9d9eb484da052002970b4b
kghugo <hugosum.dev@protonmail.com>
2022-02-07 10:41:09 +0000

    Run no curly braces rule with eslint manually

--- src/components/ui/ButtonsGroup/ButtonsGroup.test.tsx
+++ src/components/ui/ButtonsGroup/ButtonsGroup.test.tsx
@@ -4,12 +4,7 @@ import ButtonsGroup from './index';
 describe('ButtonsGroup', () => {
   it('renders with left text and right text', () => {
     render(
-      <ButtonsGroup
-        leftText={'foo'}
-        rightText={'bar'}
-        isFirstSelected
-        toggle={() => {}}
-      />
+      <ButtonsGroup leftText="foo" rightText="bar" isFirstSelected toggle={() => {}} />
     );
 
     const leftText = screen.getByText('foo');
@@ -21,12 +16,7 @@ describe('ButtonsGroup', () => {
 
   it('renders green when first selected', () => {
     render(
-      <ButtonsGroup
-        leftText={'foo'}
-        rightText={'bar'}
-        isFirstSelected
-        toggle={() => {}}
-      />
+      <ButtonsGroup leftText="foo" rightText="bar" isFirstSelected toggle={() => {}} />
     );
 
     const leftText = screen.getByText('foo');
@@ -37,8 +27,8 @@ describe('ButtonsGroup', () => {
   it('renders white when not first selected', () => {
     render(
       <ButtonsGroup
-        leftText={'foo'}
-        rightText={'bar'}
+        leftText="foo"
+        rightText="bar"
         isFirstSelected={false}
         toggle={() => {}}
       />
