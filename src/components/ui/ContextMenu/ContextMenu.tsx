864ae0ba7a27c5b0ceb13776c4222b0158979827
kghugo <hugosum.dev@protonmail.com>
2022-02-07 12:16:59 +0000

    Add components

---
+++ src/components/ui/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,91 @@
+import { memo, useCallback } from 'react';
+import type { ReactNode } from 'react';
+import { useToggle } from 'react-use';
+
+import Tooltip from '@mui/material/Tooltip';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+
+import MoreIcon from '../../../assets/more.svg';
+import CrossIcon from '../../../assets/cross.svg';
+
+import Pressable from '../Pressable';
+import Typography from '../Typography';
+import HorizontalDivider from '../HorizontalDivider';
+
+import styles from './ContextMenu.module.scss';
+
+type ContextMenuOption = {
+  icon: ReactNode;
+  label: string;
+  onClick: () => void;
+};
+
+type ContextMenuProps = {
+  options: Array<ContextMenuOption>;
+  trigger?: ReactNode;
+  testId?: string | null;
+};
+
+//  <MoreIcon />
+const ContextMenu = ({ options, trigger, testId = null }: ContextMenuProps) => {
+  const [isOpen, toggle] = useToggle(false);
+
+  const onOpen = useCallback(() => toggle(true), [toggle]);
+  const onClose = useCallback(() => toggle(false), [toggle]);
+
+  const onOptionClick = useCallback(
+    (onClick) => {
+      onClick();
+      toggle();
+    },
+    [toggle]
+  );
+
+  const tooltipTitle = (
+    <div className={styles.tooltipWrapper} data-testid="context-menu-content">
+      <div className={styles.header}>
+        <Typography size="s">More...</Typography>
+        <CrossIcon onClick={toggle} data-testid="context-menu-cross" />
+      </div>
+
+      <HorizontalDivider />
+
+      {options.map((option) => (
+        <div key={option.label} className={styles.option}>
+          {option.icon}
+
+          <Pressable
+            onClick={() => onOptionClick(option.onClick)}
+            data-testid="context-menu-option"
+          >
+            <Typography size="s">{option.label}</Typography>
+          </Pressable>
+        </div>
+      ))}
+    </div>
+  );
+
+  return (
+    <div className={styles.wrapper} data-testid={testId}>
+      <ClickAwayListener onClickAway={onClose}>
+        <div>
+          <Tooltip
+            open={isOpen}
+            onClose={onClose}
+            title={tooltipTitle}
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
+            placement="bottom-end"
+            classes={{ tooltip: styles.tooltip }}
+            PopperProps={{ disablePortal: true }}
+          >
+            <Pressable onClick={onOpen}>{trigger || <MoreIcon />}</Pressable>
+          </Tooltip>
+        </div>
+      </ClickAwayListener>
+    </div>
+  );
+};
+
+export default memo(ContextMenu);
---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,73 @@
+import classnames from 'classnames';
+
+import Typography from '../Typography';
+import ErrorMessage from '../ErrorMessage';
+import LeftBorderContainer from '../../layout/LeftBorderContainer';
+
+import styles from './Input.module.scss';
+
+type ValidationSchema = {};
+
+type InputProps = {
+  name?: string;
+  label?: string | null;
+  classNameContainer?: string | null;
+  classNameInput?: string | null;
+  errorMessage?: string;
+  register?: () => void;
+  validation?: ValidationSchema;
+  withBorder?: boolean;
+  withError?: boolean;
+  description?: string | null;
+  disabled?: boolean;
+};
+
+const Input = ({
+  label = null,
+  name = '',
+  classNameContainer = null,
+  classNameInput = null,
+  register = () => {}, // for usage with react-hook-form
+  validation = {},
+  errorMessage = '',
+  withBorder = false,
+  withError = false, // use only to color input in red
+  description = null,
+  disabled = false,
+  ...rest
+}: InputProps) => (
+  <LeftBorderContainer
+    enabled={withBorder}
+    hasError={Boolean(errorMessage)}
+    className={classNameContainer}
+  >
+    {label && (
+      <label htmlFor={name} className="govuk-label lbh-label">
+        {label}
+      </label>
+    )}
+
+    <ErrorMessage text={errorMessage} className={styles.error} />
+
+    <input
+      className={classnames('govuk-input lbh-input', classNameInput, {
+        'govuk-input--error': Boolean(errorMessage) || withError,
+        [styles.disabled]: disabled,
+      })}
+      id={name}
+      name={name}
+      type="text"
+      disabled={disabled}
+      {...rest}
+      {...register(name, validation)}
+    />
+
+    {description && (
+      <Typography size="s" className={styles.description}>
+        {description}
+      </Typography>
+    )}
+  </LeftBorderContainer>
+);
+
+export default Input;
