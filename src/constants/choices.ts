58e561bf63fb8f9a11152620e3736b16b8378a89
kghugo <hugosum.dev@protonmail.com>
2022-02-08 09:05:14 +0000

    Remove redundant

--- src/constants/choices.ts
+++
@@ -1,6 +0,0 @@
-export const STATEMENTS_TYPES = [
-  { id: 0, value: 'Tenant' },
-  { id: 1, value: 'Leasehold' },
-];
-
-export default STATEMENTS_TYPES;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,1 @@
 export * from './common';
-export * from './search';
-export * from './routes';
-export * from './choices';
-export * from './auth';
