c64938384d05938628170e0c703ac8cdfdb68961
kghugo <hugosum.dev@protonmail.com>
2022-02-08 08:39:54 +0000

    Add back in cjs import

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,15 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import packageJson from './package.json';
 import alias from '@rollup/plugin-alias';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import packageJson from './package.json';
 
 const rootPath = dirname(fileURLToPath(import.meta.url));
+
+// NOTE Has to use CJS for postcss plugin
+const autoImport = require('./autoImport');
 
 export default [
   {
@@ -29,18 +32,14 @@ export default [
     ],
     plugins: [
       alias({
-        entries: [
-          { find: '@assets', replacement: `${rootPath}/src/assets` },
-        ],
+        entries: [{ find: '@assets', replacement: `${rootPath}/src/assets` }],
       }),
       peerDepsExternal(),
       typescript({ tsconfig: './tsconfig.json' }),
       postcss({
         extract: false,
         modules: true,
-        plugins: [
-            autoImport()
-        ],
+        plugins: [autoImport()],
         use: ['sass'],
       }),
       resolve(),
