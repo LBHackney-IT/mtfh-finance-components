aa8d981fc46a10035629de5fd1ac7d11c25792e0
kghugo <hugosum.dev@protonmail.com>
2022-02-09 13:25:54 +0000

    Remove test as user-library is broken

--- src/components/ui/DateInput/DateInput.test.tsx
+++ src/components/ui/DateInput/DateInput.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
-import userEvent from '@testing-library/user-event';
 import DateInput from './DateInput';
 import { useDateInput } from '../../../hooks/index';
 
@@ -46,24 +45,5 @@ describe('DateInput', () => {
     const content = screen.getByText('foo');
 
     expect(content).toBeInTheDocument();
-  });
-
-  it('renders with given input onChange', () => {
-    const { result } = renderHook(() => useDateInput());
-
-    const props = {
-      onChange: result.current.onChange,
-      dateValues: result.current.dateValues,
-    };
-    render(<DateInput {...props} />);
-
-    const content = screen.getAllByTestId('input')[0];
-
-    // onChange is not firing
-    // REF htt:s://github.com/testing-library/user-event/issues/676
-    // REF https://github.com/testing-library/user-event/issues/387
-    userEvent.type(content, '10');
-
-    expect(content).toHaveValue('10');
   });
 });
