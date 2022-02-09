cca8df78cb6b15603392f6981fa4723ddff7e0f8
kghugo <hugosum.dev@protonmail.com>
2022-02-09 10:17:20 +0000

    Create InputSearch

---
+++ src/components/specific/InputSearch/InputSearch.stories.tsx
@@ -0,0 +1,33 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import InputSearch from './InputSearch';
+
+export default {
+  title: 'Specific/InputSearch',
+  component: InputSearch,
+  argTypes: {},
+} as ComponentMeta<typeof InputSearch>;
+
+const Template: ComponentStory<typeof InputSearch> = (args) => <InputSearch {...args} />;
+
+const base = {
+  value: 'Search value',
+  textButton: 'Search',
+  onChange: () => {}
+};
+
+export const Normal = Template.bind({});
+Normal.args = {
+    ...base
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  ...base,
+  isLoading: true
+};
+
+export const WithOutEnterEffect = Template.bind({});
+Loading.args = {
+  ...base,
+  withOnEnter: false
+};
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,14 @@
+import { useCallback } from 'react';
+
 export { default as useTabs } from './useTabs';
 export { default as useSorts } from './useSorts';
+
+type UseOnPressEnterCallback = () => void;
+
+export const useOnPressEnter = (callback: UseOnPressEnterCallback) =>
+  useCallback(
+    ({ key }) => {
+      if (key === 'Enter') callback();
+    },
+    [callback]
+  );
