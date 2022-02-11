7a974cca4a41649a07afe1ea78f6109c935b0131
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-02-11 10:13:32 +0000

    Do this later

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,25 +8,25 @@ declare module 'react-loader-spinner' {
   }
 }
 
-import type { ColumnInstance, TableInstance, TableState } from 'react-table'
-import type { PaginationControl} from './components/ui/Table/components/Pagination'
+// import type { ColumnInstance, TableInstance, TableState } from 'react-table'
+// import type { PaginationControl} from './components/ui/Table/components/Pagination'
 
-declare module 'react-table' {
-  export interface ColumnInstance<D extends object = {}>{
-    isHidden: boolean,
-    customWidth?: number,
-    isNumeric?: boolean,
-    lightWeight?: boolean
-    boldHeader?: boolean
-    bold?: boolean
-    className?: string
-  }
+// declare module 'react-table' {
+//   export interface ColumnInstance<D extends object = {}>{
+//     isHidden: boolean,
+//     customWidth?: number,
+//     isNumeric?: boolean,
+//     lightWeight?: boolean
+//     boldHeader?: boolean
+//     bold?: boolean
+//     className?: string
+//   }
 
-  export interface TableInstance<D extends object = {}> extends PaginationControl {
+//   export interface TableInstance<D extends object = {}> extends PaginationControl {
 
-  }
+//   }
 
-  export interface TableState<D extends object = {}> {
-    pageIndex: number
-  }
-}
+//   export interface TableState<D extends object = {}> {
+//     pageIndex: number
+//   }
+// }
