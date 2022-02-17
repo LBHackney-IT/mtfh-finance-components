69a412b8e452d97683c34b4ff1c0d157c8a372cc
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-02-17 11:53:00 +0000

    Add test for Sorts

--- src/components/ui/Sorts/Sorts.test.tsx
+++ src/components/ui/Sorts/Sorts.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
+import userEvent from '@testing-library/user-event';
 
 import Sorts from './index';
 import { useSorts } from '../../../hooks';
@@ -24,5 +25,60 @@ describe('Sorts', () => {
     const content = screen.getByTestId('sorts');
 
     expect(content).toHaveClass('foo');
+  });
+
+  it('renders options', () => {
+    const { result } = renderHook(() => useSorts());
+
+    const options = [
+      {
+        id: 'foo',
+        label: 'item1',
+      },
+    ];
+
+    render(<Sorts options={options} {...result.current} />);
+
+    const content = screen.getByText('item1');
+
+    expect(content).toBeInTheDocument();
+  });
+
+  it('fires onClick on click', () => {
+    const onChange = jest.fn();
+
+    const options = [
+      {
+        id: 'foo',
+        label: 'item1',
+      },
+    ];
+
+    render(<Sorts options={options} selectedOptions={new Set()} onChange={onChange} />);
+
+    const content = screen.getByText('item1');
+
+    userEvent.click(content);
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it('fires onClick on keydown Enter', () => {
+    const onChange = jest.fn();
+
+    const options = [
+      {
+        id: 'foo',
+        label: 'item1',
+      },
+    ];
+
+    render(<Sorts options={options} selectedOptions={new Set()} onChange={onChange} />);
+
+    const content = screen.getByText('item1');
+
+    userEvent.type(content, 'Enter');
+
+    expect(onChange).toHaveBeenCalled();
   });
 });
