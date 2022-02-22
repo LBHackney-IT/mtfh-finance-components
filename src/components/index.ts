5b916f0d016022635a774f0c198f830adbfeb78f
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2022-02-22 20:24:55 +0000

    Exports all components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,44 @@
-// layout
+export { default as HeaderBlock } from './blocks/HeaderBlock';
+export { default as InfoItem } from './blocks/InfoItem';
+export { default as InformationBlock } from './blocks/InformationBlock';
+
 // eslint-disable-next-line import/prefer-default-export
 export { default as GrayBox } from './layout/GrayBox';
+export { default as InfoItemsList } from './layout/InfoItemsList';
+export { default as InformationBlocksContainer } from './layout/InformationBlocksContainer';
+export { default as LeftBorderContainer } from './layout/LeftBorderContainer';
+export { default as LeftMenu } from './layout/LeftMenu';
+export { default as LeftMenuButton } from './layout/LeftMenuButton';
+export { default as PageContainer } from './layout/PageContainer';
+
+export { default as ModalButtons } from './modals/ModalButtons';
+export { default as OverlayModal } from './modals/OverlayModal';
+
+export { default as InputSearch } from './specific/InputSearch';
+
+export { Accordion } from './ui/Accordion';
+export { default as Alert } from './ui/Alert';
+export { default as AssetIcon } from './ui/AssetIcon';
+export { default as Button } from './ui/Button';
+export { default as ButtonsGroup } from './ui/ButtonsGroup';
+export { default as Checkbox } from './ui/Checkbox';
+export { default as CheckboxSmall } from './ui/CheckboxSmall';
+export { default as ContextMenu } from './ui/ContextMenu';
+export { default as DateInput } from './ui/DateInput';
+export { default as EmailLink } from './ui/EmailLink';
+export { default as ErrorMessage } from './ui/ErrorMessage';
+export { default as FileInput } from './ui/FileInput';
+export { default as Hint } from './ui/Hint';
+export { default as HorizontalDivider } from './ui/HorizontalDivider';
+export { default as Input } from './ui/Input';
+export { default as Link } from './ui/Link';
+export { default as Loader } from './ui/Loader';
+export { default as NoData } from './ui/NoData';
+export { default as Popup } from './ui/Popup';
+export { default as Pressable } from './ui/Pressable';
+export { RadioButton, RadioButtons } from './ui/Radios';
+export { default as Sorts } from './ui/Sorts';
+export { default as Table } from './ui/Table';
+export { default as Tabs } from './ui/Tabs';
+export { default as Typography } from './ui/Typography';
+export { default as VerticalDivider } from './ui/VerticalDivider';
